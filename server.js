43d5cd0c939ca0b87cc63e3f686a11bc3d73ebd7
Aibek <aibekaitan1@gmail.com>
2025-10-28 18:51:52 +0500

    Commit - 4

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const http = require('http')
 const fs = require('fs')
 let requestsCount = 0
 
+const delay = (ms)=>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve()
+        },ms)
+    })
+}
+const readFile = (path)=>{
+    return new Promise((resolve, reject)=>{
+        fs.readFile(path, (err,data)=>{
+            if(err){
+                reject('Error')
+            }
+            else{
+                resolve(data)
+            }
 
-const server = http.createServer((request, response) => {
+        })
+    })
+}
+
+
+const server = http.createServer(async (request, response) => {
     console.log(request.url)
     if(request.url === '/favicon.ico'){
         const favicon = fs.readFileSync('./favicon.ico');
@@ -17,23 +38,22 @@ const server = http.createServer((request, response) => {
 
     switch (request.url) {
         case '/students' :
-            setTimeout(()=>{
-                response.write('Hello')
-                response.end();
-            },5000)
-            // response.write('Students')
+            await delay(3000)
+            response.write('Students')
+            response.end();
+
             break
         case '/' :
         case '/courses' :
-            fs.readFile('pages.html', (err,data)=>{
-                if(err){
-                    response.write('Error')
-                }
-                else{
-                    response.write(data)
-                }
+            try {
+                const data = await readFile('pages.html')
+                response.write(data)
                 response.end()
-            })
+            }
+            catch(err){
+                response.write('something wrong, 500')
+                response.end()
+            }
             break
         default:
             response.write('404 Not Found')
