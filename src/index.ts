46142e75dec7da1b515ac4b3dea43b667672e30c
Aibek <aibekaitan1@gmail.com>
2025-11-07 19:19:19 +0500

    Commit - 8

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ const db = {
     ]
 }
 
+const HTTP_STATUSES = {
+    OK_200 : 200,
+    CREATED_201 : 201,
+    NO_CONTENT_204 : 204,
+
+    BAD_REQUEST_400 : 400,
+    NOT_FOUND_404 : 404
+}
+
 const jsonBodyMiddleware = express.json()
 app.use(jsonBodyMiddleware)
 
@@ -27,20 +36,18 @@ app.get('/courses', (req, res) => {
 app.get('/courses/:id', (req, res) => {
     const foundCourse = db.courses.find(c => c.id === +req.params.id);
     if(!foundCourse){
-        res.sendStatus(404)
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
         return ;
     }
-
-
     res.json(foundCourse)
 })
 app.delete('/courses/:id', (req, res) => {
     db.courses = db.courses.filter(c => c.id !== +req.params.id);
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 app.post('/courses', (req, res) =>{
     if(!req.body.title){
-        res.sendStatus(400)
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
         return;
     }
     const createdCourse = {
@@ -48,24 +55,24 @@ app.post('/courses', (req, res) =>{
         title: req.body.title
     };
     db.courses.push(createdCourse)
-    res.status(201).json(createdCourse)
+    res.status(HTTP_STATUSES.CREATED_201).json(createdCourse)
 
 })
 
 app.put('/courses/:id', (req, res) => {
     if(!req.body.title){
-        res.sendStatus(400)
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
         return ;
     }
 
     const foundCourse = db.courses.find(c => c.id === +req.params.id);
     if(!foundCourse){
-        res.sendStatus(404)
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
         return;
     }
     foundCourse.title = req.body.title
 
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 
 app.listen(port, () => {
